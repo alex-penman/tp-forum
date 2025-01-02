97874623a934a4fdfee966dae247fc22ff683ef7
Alexander Penman <apenman1991@gmail.com>
2025-01-02 20:54:57 +1100

    added console log re: secret key base in 100-secret_token.rb

--- config/initializers/100-secret_token.rb
+++ config/initializers/100-secret_token.rb
@@ -3,4 +3,9 @@
 # Not fussed setting secret_token anymore, that is only required for
 # backwards support of "seamless" upgrade from Rails 3.
 # Discourse has shipped Rails 3 for a very long time.
-Discourse::Application.config.secret_key_base = GlobalSetting.safe_secret_key_base
+
+# Check if the SECRET_KEY_BASE environment variable is set and fall back to GlobalSetting if needed.
+Discourse::Application.config.secret_key_base = ENV['SECRET_KEY_BASE'] || GlobalSetting.safe_secret_key_base
+
+# Debugging output to ensure the correct key is being used.
+puts "Using SECRET_KEY_BASE: #{Discourse::Application.config.secret_key_base ? 'Set' : 'Not Set'}"
